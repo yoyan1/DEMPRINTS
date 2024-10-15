2b18730451d5c85c761a91a4e6460abd100ea515
HannSammDev <beleganiohannsamm@gmail.com>
2024-10-15 09:30:35 +0800

    First commit

---
+++ app/staff/layout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Heading from "./head/heading";
+import Sidebar from "./head/aside";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <div class="antialiased bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col">
+          <Heading />
+        </div>
+        <div className="flex flex-row">
+          {/* Sidebar placed on the left */}
+          <Sidebar className="w-64 p-4" />
+          <main className="flex-grow p-4">{children}</main>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ app/staff/transaction/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Transaction(){
+    return(
+        <>
+        <div>
+            <h1 style={{marginTop:'10em'}}>HELLO</h1>
+        </div>
+        </>
+    )
+}
