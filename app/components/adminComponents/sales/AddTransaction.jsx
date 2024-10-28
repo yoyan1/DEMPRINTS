cda14ad3142696106614dd423d9a28ac1d08c582
yoyan1 <yoyan947@gmail.com>
2024-10-28 17:18:21 -0600

    transaction data in database with create order modal

--- app/components/adminComponents/sales/AddTransaction.jsx
+++ app/components/adminComponents/sales/AddTransaction.jsx
@@ -1,47 +1,116 @@
 "use client"
-import React from "react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
-import { MdAdd } from 'react-icons/md';
-// import { Input } from "@nextui-org/react";
+import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Select, SelectItem, Input} from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { MdAdd } from "react-icons/md";
+import axios from "axios";
 
 export default function CreateTransaction() {
-  const {isOpen, onOpen, onClose} = useDisclosure();
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const [products, setProducts] = useState([])
+  const [options, setOptionList] = useState([])
+  const [type, setTypeList] = useState([])
 
-  const handleOpen = () => {
-    onOpen();
+  const fetchAll = async  () =>{
+    const result = await axios.get('http://localhost:5000/api/master/products');
+    setProducts(result.data); 
+    const responseOptions = await axios.get('http://localhost:5000/api/master/getPaymentOptions')
+    setOptionList(responseOptions.data)
+    const responseType = await axios.get('http://localhost:5000/api/master/getPaymentType')
+    setTypeList(responseType.data)
   }
 
+  useEffect(() =>{
+    fetchAll()
+  }, [])
+
+  const customer_type = ['Walk in', 'Online']
   return (
     <>
-        <div className="p-md">
-            <Button color="primary" onPress={handleOpen}><MdAdd/> Order</Button>
-            <Modal 
-                size="2xl"
-                isOpen={isOpen} 
-                onClose={onClose} 
-            >
-                <ModalContent>
-                {(onClose) => (
-                    <>
-                    <ModalHeader className="flex flex-col gap-1">Create Order</ModalHeader>
-                    <ModalBody>
-                        <div>
-
-                        </div>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="danger" variant="light" onPress={onClose}>
-                        Close
-                        </Button>
-                        <Button color="primary" onPress={onClose}>
-                        Action
-                        </Button>
-                    </ModalFooter>
-                    </>
-                )}
-                </ModalContent>
-            </Modal>    
-        </div>
+      <Button onPress={onOpen} color="primary"><MdAdd/> order</Button>
+      <Modal 
+        isOpen={isOpen} 
+        onOpenChange={onOpenChange}
+        placement="top-center"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Create Order</ModalHeader>
+              <ModalBody>
+                <Select 
+                  label="Product name" 
+                  name="unit"
+                >
+                  {products.map(item => (
+                    <SelectItem key={item.name} value={item.name}>
+                      {item.name}
+                    </SelectItem>
+                  ))}
+                </Select>
+                <div className="flex gap-2">
+                    <Input
+                    autoFocus
+                    type="number"
+                    label="Quantity"
+                    placeholder="Enter quantity"
+                    variant="bordered"
+                    />
+                    <Input
+                    autoFocus
+                    label="Discount"
+                    placeholder="Enter cutomer discount"
+                    variant="bordered"
+                    />
+                </div>
+                <Select 
+                  label="Customer type" 
+                  name="unit"
+                >
+                  {customer_type.map(item => (
+                    <SelectItem key={item} value={item}>
+                      {item}
+                    </SelectItem>
+                  ))}
+                </Select>
+                <Input
+                  autoFocus
+                  label="Customer name"
+                  placeholder="Enter customer name"
+                  variant="bordered"
+                />
+                <Select 
+                  label="Payment type" 
+                  name="unit"
+                >
+                  {type.map(item => (
+                    <SelectItem key={item.name} value={item.name}>
+                      {item.name}
+                    </SelectItem>
+                  ))}
+                </Select>
+                <Select 
+                  label="Payment options" 
+                  name="unit"
+                >
+                  {options.map(item => (
+                    <SelectItem key={item.name} value={item.name}>
+                      {item.name}
+                    </SelectItem>
+                  ))}
+                </Select>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="flat" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Sign in
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 }
