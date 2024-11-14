82c3757e0f4fdc04865c630c9f024e8e025922ea
yoyan1 <yoyan947@gmail.com>
2024-11-14 16:45:19 -0600

    user authentication

--- app/admin/sales/page.jsx
+++ app/admin/sales/page.jsx
@@ -1,75 +1,108 @@
 "use client";
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import TransactionTable from '@/app/components/adminComponents/sales/TransactionTable'
 import {useSalesStore} from '@/app/stores/transactionStore'
 import AdminLayout from '../layout/layout'
 import { getDateAndTime } from '@/app/composables/dateAndTime'
 import { formatDate } from '@/app/composables/formateDateAndTime';
 import { BiLineChart, BiLineChartDown } from "react-icons/bi";
+import { DatePicker } from '@nextui-org/react';
+
+const getDateYesterday = (currentDate) => {
+  let dateNow = new Date(currentDate); 
+  dateNow.setDate(dateNow.getDate() - 1); 
+
+  let month = dateNow.getMonth() + 1;  
+  let day = dateNow.getDate();
+  let year = dateNow.getFullYear();
+
+  month = month < 10 ? '0' + month : month;
+  day = day < 10 ? '0' + day : day;
+
+  return `${year}-${day}-${month}`;
+};
+
+const getTotalSalesDaily = (transactions, date, dateNow) => {
+
+  const dateYesterday = getDateYesterday(date);
+  const dateSelected = date? date : dateNow
+  return transactions.reduce(
+    (acc, item) => {
+      if (item.date === dateSelected) {
+        acc.totalSales += item.total;
+      } else if (item.date === dateYesterday) {
+        acc.totalYesterday += item.total;
+      }
+      return acc;
+    },
+    { totalSales: 0, totalYesterday: 0 } 
+  );
+};
 
 export default function Sales() {
   const {columns, itemOptions, typeOptions, transactions, loading, fetchTransactions } = useSalesStore();
   const {date} = getDateAndTime()
+  const [selectedDate, setSelectedDate ] = useState('')
+
   useEffect(() =>{
     fetchTransactions()
   }, [fetchTransactions])
 
-  const getDateYesterday = () =>{
-    let dateNow = new Date(date);
-    dateNow.setDate(dateNow.getDate());
+  const filteredTransactions = useMemo(
+    () => transactions.filter((transaction) => transaction.date.includes(selectedDate)),
+    [transactions, selectedDate] 
+  );
 
-    let month = dateNow.getMonth() + 1;  
-    let day = dateNow.getDate();
-    let year = dateNow.getFullYear();
+  const { totalSales, totalYesterday } = useMemo(
+    () => getTotalSalesDaily(transactions, selectedDate, date),
+    [transactions, selectedDate, date] 
+  );
 
-    month = month < 10 ? '0' + month : month;
-    day = day < 10 ? '0' + day : day;
+  const handleDateChange = (date) => {
+    const year = date.year? `${date.year}-` : ''
+    const month = date.month? `${date.month}-` : ''
+    const day = date.day? date.day : ''
+    const fullDate = year+month+day
+    setSelectedDate(fullDate? fullDate : '')
+  };
 
-    let formattedDate = `${year}-${day}-${month}`;
-    return formattedDate
-  }
-  const getTotalSalesDaily = () =>{
-    let totalSales = 0
-    let totalYesterday = 0
-    transactions.map((item) =>{
-      if(item.date === date){
-        totalSales = totalSales + item.total
-      }
-       else if(item.date === getDateYesterday()){
-        totalYesterday = totalYesterday + item.total
-       }
-    })
-    
-    return {totalSales, totalYesterday}
-  }
-
-  const {totalSales, totalYesterday} = getTotalSalesDaily()
   return (
     <AdminLayout>
         <main className="flex flex-1 rounded-md flex-col gap-4 m-4 lg:gap-6 lg:m-6">
           <div className='flex flex-col gap-2'>
             <div className="flex justify-between items-start rounded-lg bg-white dark:bg-gray-900 p-5">
-              <div className='flex gap-2'>
+              <div className='flex gap-5'>
                 <div>
                   <h1 className="font-bold text-2xl">Sales Overview</h1>
                   <span className="text-sm text-slate-400">{"Let's"} see the current statistic performance</span>
                 </div>
-                <div className='flex items-end gap-5 border-2 p-3 shadow-sm rounded-xl border-blue-600 '>
-                    <span>Today's sales: </span>
-                    <span className='text-slate-400 text-sm'>{ Math.round(totalSales) }</span>
-                  { totalSales > totalYesterday? (
-                    <BiLineChart className='text-green-600 h-5 w-5'/>
-                  ): (
-                    <BiLineChartDown className='text-red-600 h-5 w-5 '/>
-                  )}
+                <div className=' border-2 p-3 shadow-sm rounded-xl border-blue-600 '>
+                    <span className='text-sm text-slate-400'>{formatDate(selectedDate? selectedDate : date)}</span>
+                    <div className='flex items-end gap-5'>
+                      <span className='font-sans font-semibold'>Sales: </span>
+                      <span className='text-slate-400 text-sm'>{ Math.round(totalSales) }</span>
+                      { totalSales > totalYesterday? (
+                        <BiLineChart className='text-green-600 h-5 w-5'/>
+                      ): (
+                        <BiLineChartDown className='text-red-600 h-5 w-5 '/>
+                      )}
+                    </div>
                 </div>
               </div>
-              <div>
-                <span className="text-sm py-1 px-2 border rounded-full">{formatDate(date)}</span>
+              <div className='w-48'>
+                <DatePicker
+                  label="Select date"
+                  variant="bordered"
+                  showMonthAndYearPickers
+                  clearable
+                  initialValue={new Date()}
+                  onChange={handleDateChange}
+                />
+                {/* <span className="text-sm py-1 px-2 border rounded-full">{formatDate(date)}</span> */}
               </div>
             </div>
             <div className='bg-white dark:bg-gray-900 rounded-lg p-5'>
-              <TransactionTable columns={columns} transactions={transactions} itemOptions={itemOptions} typeOptions={typeOptions} loading={loading} isMaximized={false} refresh={fetchTransactions}/>
+              <TransactionTable columns={columns} transactions={filteredTransactions} itemOptions={itemOptions} typeOptions={typeOptions} loading={loading} isMaximized={false} refresh={fetchTransactions}/>
             </div>
           </div>
         </main>
---
+++ app/components/login-form.jsx
@@ -0,0 +1,97 @@
+"use client"
+import React, { useEffect, useState } from "react"
+import Link from "next/link"
+import { Button } from "@nextui-org/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card"
+import { Input } from "@/app/components/ui/input"
+import { Label } from "@/app/components/ui/label"
+import { useUserStore } from "../stores/userStore"
+// import { useRouter } from 'next/router';
+
+export function LoginForm() {
+  const { login, loading } = useUserStore()
+  const [ credentials, setCredentials ] = useState({email: '', password: ''})
+  const [mounted, setMounted] = useState(false); 
+  
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  
+  
+  if (!mounted) {
+    return <div>Loading...</div>; 
+  }
+  // const router = useRouter()
+
+  const submit = async (e) =>{
+    e.preventDefault()
+    const response = await login(credentials)
+    if(!response.data.err){
+      localStorage.setItem("token", response.data.token)
+      // router.push('/test')
+    } else{
+      console.log(response.data.message)
+    }
+  }
+
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={submit}>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                value={credentials.email}
+                onChange={(e)=>(setCredentials((prevData) => ({...prevData, email: e.target.value})))}
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link href="#" className="ml-auto inline-block text-sm underline">
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input 
+              id="password"
+              type="password" 
+              required 
+              value={credentials.password} 
+              onChange={(e)=>(setCredentials((prevData) => ({...prevData, password: e.target.value})))}
+              />
+            </div>
+            <Button type="submit" className="w-full" isLoading={loading}>
+              Login
+            </Button>
+            <Button variant="outline" className="w-full">
+              Login with Google
+            </Button>
+          </div>
+        </form>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link href="#" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
