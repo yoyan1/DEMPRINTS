99d1779b161b8a5aa8862acf8c84adfebff662a7
yoyan1 <yoyan947@gmail.com>
2024-11-19 11:46:31 -0600

    authentication

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,15 @@
+"use client"
 import { LoginForm } from "@/app/components/login-form"
 import { Image } from "@nextui-org/react"
 
-export default function Page() {
+export default function LoginPage() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
       <div className="flex-1 bg-blue-200 w-[100vh] ">
         <Image
         width={300}
-        alt="NextUI hero Image"
-        src="https://nextui.org/images/hero-card-complete.jpeg"
+        alt="Demprints"
+        src="/DEMPRINTS-bg.jpg"
       />
       </div>
       <div className="flex-1">
