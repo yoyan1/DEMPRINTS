2180a55c236535c91f7a03d1bb5cffb4f33b13bc
yoyan1 <yoyan947@gmail.com>
2024-11-20 15:35:36 -0600

    admin authentication

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,74 +1,25 @@
+
 "use client"
-import React, { useEffect, useState } from 'react'
-import { useUserStore } from "../stores/userStore"
-import { useRouter } from 'next/navigation'
+import { productStore } from "../stores/productStore"
+import React, { useEffect } from 'react'
 
-export default function TestPage() {
-  const { user, loading, error, isAuthenticate, getAuthenticateUser} = useUserStore()
-  const router = useRouter()
+export default function page() {
+  const {products, error, loadProduct, fetchProducts} = productStore()
 
-  useEffect(() => {
-    const load = async() => {
-      await getAuthenticateUser()
-    }
+  useEffect(()=>{
+    fetchProducts()
+  }, [fetchProducts])
 
-    load()
-  }, [getAuthenticateUser]); 
-
-  if (loading) {
+  if(loadProduct) {
     return <div>Loading...</div>;
   }
 
-  if(!isAuthenticate && user) {
-    if(user.role === 'super admin'){
-      router.push('/damin')
-    } else if(user.role === 'staff'){
-      router.push('/staff')
-    }
+  if(error) {
+    return <div>{error}</div>;
   }
-  if(isAuthenticate) {
-    router.push('/login')
-  }
-
-
   return (
     <div>
-      {user ? (
-        <div>
-          <h1>User Info</h1>
-          <p>Role: {user.role}</p>  {/* Assuming the user has a name field */}
-          <p>Name: {user.name}</p>  {/* Assuming the user has a name field */}
-          <p>Email: {user.email}</p>  {/* Assuming the user has an email field */}
-          {/* Render more fields as necessary */}
-        </div>
-      ) : (
-        <div>No user data found</div>
-      )}
+      {products}
     </div>
-  );
+  )
 }
-
-// "use client"
-// import { productStore } from "../stores/productStore"
-// import React, { useEffect } from 'react'
-
-// export default function page() {
-//   const {products, error, loadProduct, fetchProducts} = productStore()
-
-//   useEffect(()=>{
-//     fetchProducts()
-//   }, [fetchProducts])
-
-//   if(loadProduct) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if(error) {
-//     return <div>{error}</div>;
-//   }
-//   return (
-//     <div>
-//       {products}
-//     </div>
-//   )
-// }
