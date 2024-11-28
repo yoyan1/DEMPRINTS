434fcf3ece085f9747cd83cc50c64aad2a22d158
yoyan1 <yoyan947@gmail.com>
2024-11-28 14:07:13 -0600

    Forgot password requests

--- app/components/user/forgotPassword.jsx
+++ app/components/user/forgotPassword.jsx
@@ -1,7 +1,10 @@
 "use client"
 import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input, User} from "@nextui-org/react";
 import { useUserStore } from "@/app/stores/userStore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { IoEye } from "react-icons/io5";
+import { IoIosEyeOff } from "react-icons/io";
+import axios from "axios";
 
 export default function ForgotPassword() {
   const {isOpen, onOpen, onOpenChange} = useDisclosure();
@@ -9,16 +12,55 @@ export default function ForgotPassword() {
   const [idNumber, setIdNumber] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState()
+  const [isRequest, setIsRequest] = useState(false)
+  const [isVisible, setIsVisible] = useState(false)
+  const [isVisibleConfirm, setIsVisibleConfirm] = useState(false)
+  const [password, setPassword] = useState({new: '', confirm: ''})
+  const [successMessage, setSuccesMessage] = useState("")
+
+  useEffect(()=>{
+    setIsRequest(false)
+  }, [])
 
   const onSubmit = async () => {
+    if(!idNumber){
+      setErrorMessage("Please enter your ID Number")
+      return
+    }
+
     const result = await findUser({id_number: idNumber})
     if(result.msg !== '') {
         setErrorMessage(result.msg)
-        console.log(result.msg);
         return
-    }
-    setUser(result.user)
+      }
+      setErrorMessage("")
+      setUser(result.user)
     
+  }
+
+  const [error, setError] = useState('')
+  const [loadSubmit, setLoadSubmit] = useState(false)
+  const onSubmitRequest = async () => {
+    if(password.new !== password.confirm){
+      setError("Password not matched.")
+      return
+    }
+    setLoadSubmit(true)
+    setError("")
+    
+    const newData = {
+      name: `${user.firstname} ${user.lastname}`,
+      id: user._id,
+      id_number: user.id_number,
+      password: password.new
+    }
+    const response = await axios.post(process.env.NEXT_PUBLIC_API_URL+'/users/createRequests', newData)
+    setSuccesMessage(`${response.data} Please Wait for admin confirmation.`)
+    setUser()
+    setPassword({})
+    setIsRequest(false)
+    setIdNumber("")
+    setLoadSubmit(false)
   }
 
   return (
@@ -30,6 +72,7 @@ export default function ForgotPassword() {
             <>
               <ModalHeader className="flex flex-col gap-1">Forgot Password</ModalHeader>
               <ModalBody>
+                <span className="text-center text-green-600">{successMessage}</span>
                 <Input 
                 label="Account ID Number" 
                 isInvalid={errorMessage? true : false}
@@ -39,34 +82,73 @@ export default function ForgotPassword() {
                 onChange={(e)=> (setIdNumber(e.target.value))} 
                 placeholder="Search your id number.."/>
                 <Button color="primary" onPress={onSubmit} isLoading={loading}>Search</Button>
-                {() => {
-                    if(user) {
-                        const imageUrl = `${process.env.NEXT_PUBLIC_API_URL}/users/images/${user.image_id}`;
-                        const  image = user.gender === 'male'? 'male-avatar.png' : 'female-avatar.png';
-                        
-                        <div className="flex flex-col items-start gap-5 p-2">
-                            <span>Result found</span>
-                            <User   
-                            name={`${user.firstname} ${user.lastname}`}
-                            description={(
-                                user.id_number
-                            )}
-                            avatarProps={{
-                                src: imageUrl? imageUrl : image
-                            }}
-                            />
+                {
+                  user && (
+                    <div className="flex flex-col items-start gap-5 p-2">
+                      <span>Result found</span>
+                      <User
+                        name={`${user.firstname} ${user.lastname}`}
+                        description={user.id_number}
+                        avatarProps={{
+                          src: user.image_id ? `${process.env.NEXT_PUBLIC_API_URL}/users/images/${user.image_id}` : (user.gender === 'male' ? 'male-avatar.png' : 'female-avatar.png')
+                        }}
+                      />
+                      <span className="text-primary cursor-pointer" onClick={() => setIsRequest(true)}>Request to change password?</span>
+                      {isRequest? (
+                        <div className="flex flex-col gap-3 w-full">
+                          <span>Create New Password</span>
+                          <span className="text-center text-red-500">{error}</span>
+                          <Input 
+                          type={isVisible ? "text" : "password"} 
+                          label="Password" 
+                          className="w-full"
+                          placeholder="Enter your new password"
+                          value={password.new}
+                          onChange={(e)=> setPassword((prevData) => ({...prevData, new: e.target.value}))}
+                          endContent={
+                            <button className="focus:outline-none" type="button" onClick={()=> setIsVisible(!isVisible)} aria-label="toggle password visibility">
+                              {isVisible ? (
+                                <IoIosEyeOff className="text-2xl text-default-400 pointer-events-none" />
+                              ) : (
+                                <IoEye className="text-2xl text-default-400 pointer-events-none" />
+                              )}
+                            </button>
+                          }     
+                          />
+                          <Input 
+                          type={isVisibleConfirm ? "text" : "password"}
+                          label="Confirm password" 
+                          className="w-full"
+                          placeholder="Confirm your password"
+                          value={password.confirm}
+                          onChange={(e)=> setPassword((prevData) => ({...prevData, confirm: e.target.value}))}
+                          endContent={
+                            <button className="focus:outline-none" type="button" onClick={()=> setIsVisibleConfirm(!isVisibleConfirm)} aria-label="toggle password visibility">
+                              {isVisibleConfirm ? (
+                                <IoIosEyeOff className="text-2xl text-default-400 pointer-events-none" />
+                              ) : (
+                                <IoEye className="text-2xl text-default-400 pointer-events-none" />
+                              )}
+                            </button>
+                          }     
+                          />
                         </div>
-                     }
-                }}
+                      ) : null}
+                    </div>
+                  )
+                }
+
               </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter>
+              {isRequest? (
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    Cancel
+                  </Button>
+                  <Button color="primary" onPress={onSubmitRequest}>
+                    Send Request
+                  </Button>
+                </ModalFooter>
+              ) : null}
             </>
           )}
         </ModalContent>
