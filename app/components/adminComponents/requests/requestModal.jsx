b3f93dfe0ce0c46109e47780c757af2adcecff6b
yoyan1 <yoyan947@gmail.com>
2024-12-03 12:26:52 -0600

    user request change password: accept and declined by admin

--- app/components/adminComponents/requests/requestModal.jsx
+++ app/components/adminComponents/requests/requestModal.jsx
@@ -1,18 +1,36 @@
 "use client"
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem, Button, User} from "@nextui-org/react";
 import { IoNotificationsCircle } from "react-icons/io5";
 import { FaCodePullRequest } from "react-icons/fa6";
 import { useRequestStore } from "@/app/stores/requestStore"
-import Action from './Action'
+import { FaCheck } from "react-icons/fa";
+import { IoCloseSharp } from "react-icons/io5";
+import axios from "axios";
 
 export default function Requests() {
   const iconClasses = "text-xl text-default-500 pointer-events-none flex-shrink-0";
-  const { requestData, fetchRequest } = useRequestStore()
+  const { requestData, fetchRequest, } = useRequestStore()
 
   useEffect(()=> {
     fetchRequest()
-  }, [requestData])
+  }, [fetchRequest])
+
+  const [loading, setLoading] = useState(false)
+
+  const onSubmit = async (data, isAccept) => {
+    setLoading(true)
+    if(isAccept){
+      const result = await axios.post(process.env.NEXT_PUBLIC_API_URL+"/users/acceptRequest", {id: data._id, password: data.password});
+      console.log(result)
+      await axios.delete(process.env.NEXT_PUBLIC_API_URL+"/users/deleteRequests/"+data._id);
+    } else{
+      await axios.delete(process.env.NEXT_PUBLIC_API_URL+"/users/deleteRequests/"+data._id);
+    }
+
+    fetchRequest()
+    setLoading(false)
+  }
 
   return (
     <Dropdown>
@@ -31,7 +49,16 @@ export default function Requests() {
             key={row.id}
             description="Request changing password"
             startContent={<FaCodePullRequest className={iconClasses} />}
-            endContent={<Action data={row} refresh={fetchRequest}/>}
+            endContent={
+              <div className="flex">
+                <Button isIconOnly color="success" isLoading={loading} onPress={() =>onSubmit(row, true)} variant="light">
+                  <FaCheck />
+                </Button>
+                <Button isIconOnly color="danger" isLoading={loading} onPress={() =>onSubmit(row, false)} variant="light">
+                  <IoCloseSharp/>
+                </Button>
+              </div>
+            }
           >
             {row.name}
           </DropdownItem>
